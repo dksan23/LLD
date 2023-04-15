46954dc86760676ab0e182a4cc28d49183ac17d7
divyansh khare <divyanshkhare26@gmail.com>
2023-04-15 18:38:59 +0530

    Added dk_cabs spring boot proj

---
+++ dk_cabs/src/main/java/com/divyansh_cab_booker/dk_cabs/DkCabsApplication.java
@@ -0,0 +1,13 @@
+package com.divyansh_cab_booker.dk_cabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkCabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkCabsApplication.class, args);
+	}
+
+}
---
+++ dk_cabs/src/test/java/com/divyansh_cab_booker/dk_cabs/DkCabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyansh_cab_booker.dk_cabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkCabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
